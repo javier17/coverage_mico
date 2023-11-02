8ef5b1bcf54ea3b46f17f85644924337b515c5de
Carlos Javier Pita Amaya <c.pita@uniandes.edu.co>
2023-11-01 20:57:47 -0500

    Se agrega modulo flask-cors

--- micro_auth/src/main.py
+++ micro_auth/src/main.py
@@ -1,4 +1,3 @@
-
 import os
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
@@ -6,7 +5,9 @@ from flask_restful import Api
 from micro_auth.src import create_app
 from micro_auth.src.models.models import db
 
+# Clave secreta insegura (NO DEBERÍAS USARLA EN PRODUCCIÓN)
 app = create_app('default')
+app.config['SECRET_KEY'] = 'clave_insegura'
 app_context = app.app_context()
 app_context.push()
 db.init_app(app)
@@ -14,11 +15,10 @@ try:
     db.create_all()
 except:
     print('already exist')
-    
+
 api = Api(app)
 jwt = JWTManager(app)
 
 print('inicio auth')
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True,
-            port=os.environ.get('APP_PORT', '5003'))
+    app.run(host="0.0.0.0", debug=True, port=os.environ.get('APP_PORT', '5003'))
