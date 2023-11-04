395ab827e600cc0f6151ff723d2b23cff0fe8b75
Carlos Javier Pita Amaya <c.pita@uniandes.edu.co>
2023-11-04 09:50:10 -0500

    Ajuste issue 12 para entrega de actividad

--- micro_project/src/controllers/app_controller.py
+++ micro_project/src/controllers/app_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from micro_project.src.utils.decorators import *
+from micro_project.src.utils.decorators import handle_error
 
 mod = Blueprint('app_controller', __name__)
 base_url = '/projects'
